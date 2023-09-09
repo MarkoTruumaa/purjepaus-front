4f8a25a3d733c12dd51a9cea394a8d8afdf5f182
MarkoTruumaa <truumaamarko@gmail.com>
2023-09-09 13:19:30 +0300

    bootstrap installed

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import axios from "axios";
 import VueAxios from "vue-axios";
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 createApp(App).use(router).mount('#app')
 Vue.use(VueAxios, axios)
+
+import 'bootstrap/dist/js/bootstrap.js'
