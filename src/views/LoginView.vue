9e941057b92103727aca7df492d45fb7a5542322
opilane <opilane@gmail.com>
2023-09-11 12:26:36 +0300

    PUR-2 WIP

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,73 @@
 <template>
-  <h1>Login</h1>
+  <h1>Sisse logimine</h1>
+  <div class="container text-center">
+    <div class="row justify-content-center">
+      <div class="col col-3 form-floating mb-4">
+        <div class="form-floating mb-3">
+          <input v-model="username" type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Kasutajanimi</label>
+        </div>
+        <div class="form-floating">
+          <input v-model="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Parool</label>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col col-3 form-floating mb-3">
+        <div class="mb-4">
+          <button @click="login" type="button" class="btn btn-secondary">Logi sisse</button>
+        </div>
+        <div class="mb-5">
+          <button type="button" class="btn btn-secondary">Loo uus kasutaja</button>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col">
+        <h3>Siia alla tuleb kontantinfo</h3>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
-  name: 'LoginView'
+  name: 'LoginView',
+  data() {
+    return {
+      username: '',
+      password: '',
+      loginResponse: {
+        userId: 0,
+        roleName: ''
+      },
+      errorResponse: {
+        message: '',
+        errorCode: 0
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$http.get('/login', {
+            params: {
+              username: this.username,
+              password: this.password
+            }
+          }
+      ).then(response => {
+        this.loginResponse = response.data
+      }).catch(error => {
+        this.errorResponse = error.response.data
+        if (this.errorResponse.errorCode !== 111) {
+          router.push({name: 'errorRoute'})
+        }
+      })
+    },
+  }
 }
 
 </script>
