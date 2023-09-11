18294ff46ed373a308c33ce1e949b51ea7cce3c0
MarkoTruumaa <truumaamarko@gmail.com>
2023-09-11 21:01:50 +0300

    MON-11 completed

---
+++ src/components/HarbourDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Valitud sadama ID = {{selectedHarbourId}}</h2>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HarbourDetails',
+  props: {
+    selectedHarbourId: Number
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
