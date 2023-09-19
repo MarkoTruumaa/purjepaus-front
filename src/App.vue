ebe22ddfe64d7acdc5b466c1a3a326398b874a62
harvut <helle.tomberg@gmail.com>
2023-09-19 08:41:14 +0300

    various changes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-dark bg-black ">
     <div class="container d-flex flex-row">
       <router-link to="/" class="navbar-brand p-0">
-        <img src="./assets/purjepaus-black.png" style="height: 30px;" alt="logo">
+        <img src="./assets/purjepaus-black.png" style="height: 35px;" alt="logo">
       </router-link>
       <div class="position-relative flex-fill">
         <div class="position-absolute w-100 d-flex justify-content-evenly" style="bottom: -40px">
@@ -18,11 +18,12 @@
           <router-link class="text-dark fw-bold btn btn-light shadow-sm rounded-0"
                        tag="button" to="/login">Sisse
           </router-link>
+          <router-link class="text-dark fw-bold btn btn-light shadow-sm rounded-0" to="/profile">
+            <font-awesome-icon :icon="['fas', 'user']"/>
+          </router-link>
         </div>
       </div>
-      <router-link class="text-white fw-bold" to="/profile">
-        <font-awesome-icon :icon="['fas', 'user']"/>
-      </router-link>
+
     </div>
   </nav>
   <router-view class="p-4 flex-fill"/>
