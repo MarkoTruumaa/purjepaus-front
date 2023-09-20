72b97c1749e69a547bb43ebe4186b7e0eefdf8ed
Kaisa <kaisa@knight.ee>
2023-09-20 13:54:28 +0300

    KAISA-PUR-30

--- src/views/CreateNewUserView.vue
+++ src/views/CreateNewUserView.vue
@@ -1,147 +1,148 @@
 <template>
-  <div>
-    <div class="margin mt-3">
-      <h1>Loo uus kasutaja</h1>
-    </div>
-    <div v-if="successMessage" class="alert-success">
-      <div class="text-bg-success p-3">{{ successMessage }}</div>
-    </div>
-    <div v-if="errorMessage" class="alert-warning">
-      <div class="text-bg-danger p-3">{{ errorMessage }}</div>
-    </div>
-    <div class="mb-3">
-      <div class="row">
-        <div class="col">
-          <label for="Eesnimi" class="form-label">Eesnimi
-            <font-awesome-icon :icon="['fas', 'asterisk']"/>
-          </label>
-        </div>
-        <div class="col">
-          <input v-model="userData.contactFirstName" type="text" class="form-control" id="Eesnimi">
-        </div>
+  <div class="container p-4 glass-background">
+    <div>
+      <div class="margin mt-3">
+        <h1>Loo uus kasutaja</h1>
       </div>
-    </div>
-    <div class="mb-3">
-      <div class="row">
-        <div class="col">
-          <label for="Perekonnanimi" class="form-label">Perekonnanimi
-            <font-awesome-icon :icon="['fas', 'asterisk']"/>
-          </label>
-        </div>
-        <div class="col">
-          <input v-model="userData.contactLastName" type="text" class="form-control" id="Perekonnanimi">
-        </div>
+      <div v-if="successMessage" class="alert-success">
+        <div class="text-bg-success p-3">{{ successMessage }}</div>
       </div>
-    </div>
-    <div class="mb-3">
-      <div class="row">
-        <div class="col col-6">
-          <label for="E-mail" class="form-label">E-mail
-            <font-awesome-icon :icon="['fas', 'asterisk']"/>
-          </label>
-        </div>
-        <div class="col">
-          <input v-model="userData.contactEmail" type="text" class="form-control" id="E-mail">
-        </div>
+      <div v-if="errorMessage" class="alert-warning">
+        <div class="text-bg-danger p-3">{{ errorMessage }}</div>
       </div>
-    </div>
-    <div class="mb-3">
-      <div class="row">
-        <div class="col col-6">
-          <label for="Kasutajanimi" class="form-label">Kasutajanimi
-            <font-awesome-icon :icon="['fas', 'asterisk']"/>
-          </label>
-        </div>
-        <div class="col">
-          <input v-model="userData.username" type="text" class="form-control" id="Kasutajanimi">
-        </div>
-      </div>
-    </div>
-    <div class="">
       <div class="mb-3">
         <div class="row">
-          <div class="col col-6">
-            <label for="Parool" class="form-label">Parool
+          <div class="col">
+            <label for="Eesnimi" class="form-label">Eesnimi
               <font-awesome-icon :icon="['fas', 'asterisk']"/>
             </label>
           </div>
           <div class="col">
-            <input v-model="userData.password" type="password" class="form-control" id="Parool">
+            <input v-model="userData.contactFirstName" type="text" class="form-control" id="Eesnimi">
           </div>
         </div>
       </div>
       <div class="mb-3">
         <div class="row">
-          <div class="col col-6">
-            <label for="Korda parooli" class="form-label">Korda parooli
+          <div class="col">
+            <label for="Perekonnanimi" class="form-label">Perekonnanimi
               <font-awesome-icon :icon="['fas', 'asterisk']"/>
             </label>
           </div>
           <div class="col">
-            <input v-model="passwordConfirmation" type="password" class="form-control" id="Parool uuesti">
-          </div>
-        </div>
-      </div>
-      <div class="mb-3">
-        <div class="row">
-          <div class="col">
-            <label for="Aadress" class="form-label">Aadress</label>
-          </div>
-          <div class="col">
-            <input v-model="userData.contactAddress" type="text" class="form-control" id="Aadress">
+            <input v-model="userData.contactLastName" type="text" class="form-control" id="Perekonnanimi">
           </div>
         </div>
       </div>
       <div class="mb-3">
         <div class="row">
           <div class="col col-6">
-            <label for="Telefon" class="form-label">Telefon</label>
+            <label for="E-mail" class="form-label">E-mail
+              <font-awesome-icon :icon="['fas', 'asterisk']"/>
+            </label>
           </div>
           <div class="col">
-            <input v-model="userData.contactTelephone" type="text" class="form-control" id="Telefon">
+            <input v-model="userData.contactEmail" type="text" class="form-control" id="E-mail">
           </div>
         </div>
       </div>
-    </div>
-    <div class="mb-3">
-      <div class="row">
-        <div class="col col-6">
+      <div class="mb-3">
+        <div class="row">
+          <div class="col col-6">
+            <label for="Kasutajanimi" class="form-label">Kasutajanimi
+              <font-awesome-icon :icon="['fas', 'asterisk']"/>
+            </label>
+          </div>
+          <div class="col">
+            <input v-model="userData.username" type="text" class="form-control" id="Kasutajanimi">
+          </div>
         </div>
-        <div class="col">
-          <div v-if="isLoggedIn" class="btn-group">
-            <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-              {{
-                {
-                  user: 'Sinu roll on Kasutaja',
-                  captain: 'Sinu roll on Kapten',
-                  admin: 'Sinu roll on Admin'
-                }[userData.roleName] || 'Vali Roll'
-              }}
-            </button>
-            <div>
-
+      </div>
+      <div class="">
+        <div class="mb-3">
+          <div class="row">
+            <div class="col col-6">
+              <label for="Parool" class="form-label">Parool
+                <font-awesome-icon :icon="['fas', 'asterisk']"/>
+              </label>
             </div>
-            <ul class="dropdown-menu">
-              <li>
-                <button type="button" class="dropdown-item" v-on:click="userData.roleName='user'">Kasutaja</button>
-              </li>
-              <li>
-                <button type="button" class="dropdown-item" v-on:click="userData.roleName='captain'">Kapten</button>
-              </li>
-              <li>
-                <button type="button" class="dropdown-item" v-on:click="userData.roleName='admin'">Admin</button>
-              </li>
-            </ul>
-          </div>
-          <div style="margin-top: 15px">
-            <button @click="addNewUser" type="button" class="btn btn-success">Loo uus kasutaja</button>
+            <div class="col">
+              <input v-model="userData.password" type="password" class="form-control" id="Parool">
+            </div>
           </div>
         </div>
+        <div class="mb-3">
+          <div class="row">
+            <div class="col col-6">
+              <label for="Korda parooli" class="form-label">Korda parooli
+                <font-awesome-icon :icon="['fas', 'asterisk']"/>
+              </label>
+            </div>
+            <div class="col">
+              <input v-model="passwordConfirmation" type="password" class="form-control" id="Parool uuesti">
+            </div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <div class="row">
+            <div class="col">
+              <label for="Aadress" class="form-label">Aadress</label>
+            </div>
+            <div class="col">
+              <input v-model="userData.contactAddress" type="text" class="form-control" id="Aadress">
+            </div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <div class="row">
+            <div class="col col-6">
+              <label for="Telefon" class="form-label">Telefon</label>
+            </div>
+            <div class="col">
+              <input v-model="userData.contactTelephone" type="text" class="form-control" id="Telefon">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mb-3">
+        <div class="row">
+          <div class="col col-6">
+          </div>
+          <div class="col">
+            <div v-if="isLoggedIn" class="btn-group">
+              <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                {{
+                  {
+                    user: 'Sinu roll on Kasutaja',
+                    captain: 'Sinu roll on Kapten',
+                    admin: 'Sinu roll on Admin'
+                  }[userData.roleName] || 'Vali Roll'
+                }}
+              </button>
+              <div>
+
+              </div>
+              <ul class="dropdown-menu">
+                <li>
+                  <button type="button" class="dropdown-item" v-on:click="userData.roleName='user'">Kasutaja</button>
+                </li>
+                <li>
+                  <button type="button" class="dropdown-item" v-on:click="userData.roleName='captain'">Kapten</button>
+                </li>
+                <li>
+                  <button type="button" class="dropdown-item" v-on:click="userData.roleName='admin'">Admin</button>
+                </li>
+              </ul>
+            </div>
+            <div style="margin-top: 15px">
+              <button @click="addNewUser" type="button" class="btn btn-success">Loo uus kasutaja</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
-
+  </div>
 
 
 </template>
@@ -251,7 +252,8 @@ input {
 
 .col {
   position: relative;
-  left: 550px;
+  left: 275px;
+
 }
 
 .btn {
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -4,26 +4,26 @@
     <div class="align-content-center">
       <div class="row justify-content-center">
         <div class="col col-4">
-          <AlertDanger :alert-message="errorMessage" />
-          <AlertSuccess :alert-message="successMessage" />
+          <AlertDanger :alert-message="errorMessage"/>
+          <AlertSuccess :alert-message="successMessage"/>
         </div>
       </div>
       <div class="card card-w-margin">
-        <ProfileInfo v-if="isView" :contact-info="contactInfo" ref="profileInfoRef" />
+        <ProfileInfo v-if="isView" :contact-info="contactInfo" ref="profileInfoRef"/>
         <ProfileInfoUpdate
-          @event-update-error-message="displayMessage"
-          @event-update-profile-view="displayProfileInfoComponent"
-          ref="profileInfoUpdateRef"
+            @event-update-error-message="displayMessage"
+            @event-update-profile-view="displayProfileInfoComponent"
+            ref="profileInfoUpdateRef"
         />
       </div>
     </div>
 
     <div class="text-center">
       <button
-        v-if="isView"
-        @click="displayProfileInfoUpdateComponent"
-        type="button"
-        class="btn btn-warning"
+          v-if="isView"
+          @click="displayProfileInfoUpdateComponent"
+          type="button"
+          class="btn btn-warning"
       >
         Muuda andmeid
       </button>
@@ -34,9 +34,16 @@
           </button>
         </div>
       </div>
-      <button v-if="!isView" @click="updateUserInfo" type="button" class="btn btn-warning">
-        Uuenda
-      </button>
+      <div>
+        <button v-if="!isView" @click="updateUserInfo" type="button" class="btn btn-warning">
+          Uuenda
+        </button>
+      </div>
+      <div>
+        <button v-if="isView" @click="displayCreateNewUserView" type="button" class="btn btn-warning">
+          Loo uus kasutaja
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -46,12 +53,18 @@ import router from '@/router'
 import ProfileInfo from '@/components/profile/ProfileInfo.vue'
 import ProfileInfoUpdate from '@/components/profile/ProfileInfoUpdate.vue'
 import AlertDanger from '@/components/AlertDanger.vue'
-import { USER_INFO_UPDATE_ERROR, USER_INFO_UPDATED } from '@/assets/script/AlertMessage'
+import {USER_INFO_UPDATE_ERROR, USER_INFO_UPDATED} from '@/assets/script/AlertMessage'
 import AlertSuccess from '@/components/AlertSuccess.vue'
+import createNewUserView from "@/views/CreateNewUserView.vue";
 
 export default {
   name: 'ProfileView',
-  components: { AlertSuccess, AlertDanger, ProfileInfoUpdate, ProfileInfo },
+  computed: {
+    createNewUserView() {
+      return createNewUserView
+    }
+  },
+  components: {AlertSuccess, AlertDanger, ProfileInfoUpdate, ProfileInfo},
 
   data() {
     return {
@@ -72,17 +85,21 @@ export default {
   methods: {
     getUserInfo() {
       this.$http
-        .get('/user', {
-          params: {
-            userId: sessionStorage.getItem('userId'),
-          },
-        })
-        .then((response) => {
-          this.contactInfo = response.data
-        })
-        .catch((error) => {
-          router.push({ name: 'errorRoute' })
-        })
+          .get('/user', {
+            params: {
+              userId: sessionStorage.getItem('userId'),
+            },
+          })
+          .then((response) => {
+            this.contactInfo = response.data
+          })
+          .catch((error) => {
+            router.push({name: 'errorRoute'})
+          })
+    },
+
+    displayCreateNewUserView() {
+      router.push({name: 'createNewUserRoute'})
     },
 
     updateUserInfo() {
