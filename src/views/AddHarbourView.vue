06223fa78b8a08dfe324af3d318421a43327ada3
opilane <opilane@gmail.com>
2023-09-20 10:46:14 +0300

    TUE-19

--- src/views/AddHarbourView.vue
+++ src/views/AddHarbourView.vue
@@ -3,6 +3,10 @@
     <AddExtrasModal ref="addExtrasModalRef"/>
     <div class="container">
       <h1>Sadama lisamine</h1>
+      <div class="col col-4">
+        <AlertDanger :alert-message="errorMessage"/>
+        <AlertSuccess :alert-message="successMessage"/>
+      </div>
       <div>
         <div class="input-group mb-3">
           <span class="input-group-text">Sadama nimi: </span>
@@ -10,9 +14,10 @@
                  aria-describedby="basic-addon1">
         </div>
         <div class="input-group mb-3">
-          <select class="form-select mb-3" aria-label="Default select example">
-            <option selected :value="0">Kõik maakonnad</option>
-            <option v-for="county in countyInfo" :value="county.countyId" :key="county.countyId">
+          <select v-model="selectedCountyName" class="form-select mb-3"
+                  aria-label="Default select example">
+            <option>Kõik maakonnad</option>
+            <option v-for="county in countyInfo" :value="county.countyName" :key="county.countyId">
               {{ county.countyName }}
             </option>
           </select>
@@ -35,11 +40,13 @@
                  aria-describedby="basic-addon1">
         </div>
         <div class="input-group mb-3">
-          <span class="input-group-text" id="basic-addon1">Kapteni nimi: </span>
-
-          <input disabled type="text" class="form-control"
-                 placeholder="eesnimi">
-          <input disabled type="text" class="form-control" placeholder="perekonnanimi">
+          <span class="input-group-text">Kapteni nimi: </span>
+          <select v-model="selectedCaptainContactId" class="form-select" aria-label="Default select example">
+            <option selected :value="0">Vali kapten</option>
+            <option v-for="captain in captainInfo" :value="captain.contactId" :key="captain.contactId">
+              {{ captain.contactFirstName }} {{ captain.contactLastName }}
+            </option>
+          </select>
         </div>
         <div class="input-group mb-3">
           <span class="input-group-text" id="basic-addon1">Kohtade arv: </span>
@@ -47,9 +54,7 @@
         </div>
         <div>
           <div>
-            <h4>
-              Kontaktandmed
-            </h4>
+            <h4>Kontaktandmed</h4>
           </div>
           <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">Muuda aadressi: </span>
@@ -87,13 +92,19 @@
           <div class="row">
 
             <div class="col">
-              <button @click="openAddExtrasModal" type="button" class="btn btn-secondary text-light fw-bold shadow-sm rounded-0 ">Lisa teenused</button>
+              <button @click="openAddExtrasModal" type="button"
+                      class="btn btn-secondary text-light fw-bold shadow-sm rounded-0 ">Lisa teenused
+              </button>
             </div>
             <div class="col">
-              <button @click="sendAddNewHarbourInfoRequest" type="button" class="btn btn-success text-light fw-bold shadow-sm rounded-0">Lisa sadam</button>
+              <button @click="addNewHarbour" type="button"
+                      class="btn btn-success text-light fw-bold shadow-sm rounded-0">Lisa sadam
+              </button>
             </div>
             <div class="col">
-              <button @click="moveToHarboursPage" type="button" class="btn btn-primary text-light fw-bold shadow-sm rounded-0">Tagasi sadamate lehele</button>
+              <button @click="moveToHarboursPage" type="button"
+                      class="btn btn-primary text-light fw-bold shadow-sm rounded-0">Tagasi sadamate lehele
+              </button>
             </div>
           </div>
         </div>
@@ -105,29 +116,45 @@
 <script>
 import router from "@/router";
 import AddExtrasModal from "@/components/modal/AddExtrasModal.vue";
+import AlertDanger from "@/components/AlertDanger.vue";
+import AlertSuccess from "@/components/AlertSuccess.vue";
+import {HARBOUR_LOCATION_ADDED, NEW_HARBOUR_ERROR} from "@/assets/script/AlertMessage";
+import {HARBOUR_NAME_UNAVAILABLE} from "@/assets/script/ErrorCode";
 
 
 export default {
   name: 'AddHarbourView',
-  components: {AddExtrasModal},
+  components: {AlertSuccess, AlertDanger, AddExtrasModal},
   data() {
     return {
+      selectedCaptainContactId: 0,
+      selectedCountyName: 'Kõik maakonnad',
+      errorMessage: '',
+      successMessage: '',
+      captainInfo: {
+        contactId: 0,
+        contactFirstName: '',
+        contactLastName: ''
+      },
       extraInfo: {
         extraId: 0,
         extraName: '',
-        isAvailable: true
+        isAvailable: false
       },
-      countyInfo: [{
+      countyInfo: {
         countyId: 0,
         countyName: ''
-      }],
+      },
+      errorResponse: {
+        message: '',
+        errorCode: 0
+      },
       harbourDetailedInfo: {
-        harbourId: 0,
         locationAddress: '',
         locationLongitude: 0,
         locationLatitude: 0,
         contactId: 0,
-        countyName: '',
+        locationCountyName: '',
         harbourName: '',
         homepage: '',
         navigationStart: '',
@@ -149,8 +176,7 @@ export default {
             pictureData: ''
           }
         ],
-      },
-      selectedCountyId: 0,
+      }
     }
   },
   methods: {
@@ -170,25 +196,76 @@ export default {
         router.push({name: 'errorRoute'})
       })
     },
-    openAddExtrasModal() {
-      this.$refs.addExtrasModalRef.openModal({
-        extraId: this.extraInfo.extraId
-      });
+    getCaptainInfo() {
+      this.$http.get("/user/captain")
+          .then(response => {
+            this.captainInfo = response.data
+          })
+          .catch(error => {
+            router.push({name: 'errorRoute'})
+          })
     },
+    openAddExtrasModal() {
+      this.$refs.addExtrasModalRef.openModal();
+    },
+
     moveToHarboursPage() {
       router.push({name: 'harboursRoute'})
     },
-    sendAddNewHarbourInfoRequest() {
-      this.$http.post("/harbour", this.harbourDetailedInfo).then(response => {
-        const responseBody = response.data
+
+    addNewHarbour() {
+      this.harbourDetailedInfo.locationCountyName = this.selectedCountyName
+      this.harbourDetailedInfo.contactId = this.selectedCaptainContactId
+      this.harbourDetailedInfo.extras = this.$refs.addExtrasModalRef.extraInfo
+      this.errorMessage = ''
+      if (this.mandatoryFieldsAreFilled()) {
+        this.sendAddNewHarbourRequest();
+      } else {
+        this.errorMessage = NEW_HARBOUR_ERROR
+      }
+
+    },
+
+    mandatoryFieldsAreFilled() {
+      let info = this.harbourDetailedInfo
+      return info.harbourName.length > 0 && info.locationAddress.length > 0 && info.minWidth > 0 &&
+          info.minDepth > 0 && info.spots > 0 && info.phoneNumber.length > 0 &&
+          info.locationCountyName !== 'Kõik maakonnad' && info.contactId !== 0
+    },
+
+    sendAddNewHarbourRequest() {
+      this.$http.post("/harbour", this.harbourDetailedInfo
+      ).then(response => {
+        this.successMessage = HARBOUR_LOCATION_ADDED.replace("?", this.harbourDetailedInfo.harbourName)
+        setTimeout(() => { this.resetAllFields()
+        }, 3000)
       }).catch(error => {
-        const errorResponseBody = error.response.data
+        this.errorResponse = error.response.data
+        this.handleErrorResponse()
       })
     },
+
+    resetAllFields() {
+      this.harbourDetailedInfo = []
+      this.selectedCaptainContactId = 0
+      this.selectedCountyName = 'Kõik maakonnad'
+      this.successMessage = ''
+    },
+
+    handleErrorResponse() {
+      if (this.errorResponse.errorCode === HARBOUR_NAME_UNAVAILABLE) {
+        this.errorMessage = this.errorResponse.message
+      } else {
+        router.push({name: 'errorRoute'})
+      }
+    },
+
   },
+
   mounted() {
     this.getExtrasInfo()
     this.getCountyInfo()
+    this.getCaptainInfo()
   }
 }
 
